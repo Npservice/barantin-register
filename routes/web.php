92067bac24af775d47c5e7082b3d2e8ee53ddb9d
Npservice <110673469+Npservice@users.noreply.github.com>
2024-02-07 22:12:41 +0700

    first commit

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::view('/user/register', 'user.register.index');
+Route::view('/admin/template', 'welcome');
+Route::view('/user/datatable', 'user.log.index');
+Route::view('/admin/datatable', 'admin.data.index');
+Route::view('login', 'auth.index');
