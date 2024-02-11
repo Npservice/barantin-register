28fee940a359d8f80b64e94c4c52b3de66f4f204
Npservice <110673469+Npservice@users.noreply.github.com>
2024-02-11 12:29:10 +0700

    perorang 30%

--- resources/views/user/register/form/perorangan.blade.php
+++ resources/views/user/register/form/perorangan.blade.php
@@ -29,98 +29,134 @@
     <div class="tab-content twitter-bs-wizard-tab-content">
         <div class="tab-pane" id="seller-details">
             <form>
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="basicpill-firstname-input">First
-                                name</label>
-                            <input type="text" class="form-control" id="basicpill-firstname-input">
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="basicpill-lastname-input">Last
-                                name</label>
-                            <input type="text" class="form-control" id="basicpill-lastname-input">
-                        </div>
+                <div class="row mb-3">
+                    <label for="upt" class="col-sm-2 col-form-label">UPT</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="UPT" id="upt" name="upt">
                     </div>
                 </div>
 
-                <div class="row">
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="basicpill-phoneno-input">Phone</label>
-                            <input type="text" class="form-control" id="basicpill-phoneno-input">
-                        </div>
-                    </div>
-                    <div class="col-lg-6">
-                        <div class="mb-3">
-                            <label class="form-label" for="basicpill-email-input">Email</label>
-                            <input type="email" class="form-control" id="basicpill-email-input">
-                        </div>
+                <div class="row mb-3">
+                    <label for="jenis_identitas" class="col-sm-2 col-form-label">Jenis Identitas</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Jenis Identitas" id="jenis_identitas"
+                            name="jenis_identitas">
                     </div>
                 </div>
-                <div class="row">
-                    <div class="col-lg-12">
-                        <div class="mb-3">
-                            <label class="form-label" for="basicpill-address-input">Address</label>
-                            <textarea id="basicpill-address-input" class="form-control" rows="2"></textarea>
-                        </div>
+
+                <div class="row mb-3">
+                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Alamat" id="alamat" name="alamat">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="negara" class="col-sm-2 col-form-label">Negara</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Negara" id="negara" name="negara">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="provinsi" class="col-sm-2 col-form-label">Provinsi</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Provinsi" id="provinsi"
+                            name="provinsi">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="kota" class="col-sm-2 col-form-label">Kota</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Kota" id="kota" name="kota">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="kecamatan" class="col-sm-2 col-form-label">Kecamatan</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Kecamatan" id="kecamatan"
+                            name="kecamatan">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="telephone" class="col-sm-2 col-form-label">Telephone</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Telephone" id="telephone"
+                            name="telephone">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="fax" class="col-sm-2 col-form-label">Fax</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="text" placeholder="Fax" id="fax" name="fax">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="email" class="col-sm-2 col-form-label">Email</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="email" placeholder="Email" id="email"
+                            name="email">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="logo_perusahaan" class="col-sm-2 col-form-label">Logo Perusahaan</label>
+                    <div class="col-sm-10">
+                        <input class="form-control" type="file" id="logo_perusahaan" name="logo_perusahaan">
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="lingkup_akivitas" class="col-sm-2 col-form-label">Lingkup Akivitas</label>
+                    <div class="col-sm-10">
+                        <textarea class="form-control" id="lingkup_akivitas" rows="3" placeholder="Lingkup Akivitas"
+                            name="lingkup_akivitas"></textarea>
+                    </div>
+                </div>
+
+                <div class="row mb-3">
+                    <label for="status_import" class="col-sm-2 col-form-label">Status Import</label>
+                    <div class="col-sm-10">
+                        <select class="form-select" id="status_import" name="status_import">
+                            <option selected disabled>Pilih Status</option>
+                            <option value="import">Import</option>
+                            <option value="tidak_import">Tidak Import</option>
+                        </select>
                     </div>
                 </div>
             </form>
+
+
         </div>
         <div class="tab-pane" id="company-document">
             <div>
                 <form>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-pancard-input">PAN
-                                    Card</label>
-                                <input type="text" class="form-control" id="basicpill-pancard-input">
-                            </div>
-                        </div>
-
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-vatno-input">VAT/TIN
-                                    No.</label>
-                                <input type="text" class="form-control" id="basicpill-vatno-input">
-                            </div>
+                    <div class="row mb-3">
+                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" placeholder="Nama" id="nama"
+                                name="nama">
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-cstno-input">CST
-                                    No.</label>
-                                <input type="text" class="form-control" id="basicpill-cstno-input">
-                            </div>
-                        </div>
 
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-servicetax-input">Service
-                                    Tax No.</label>
-                                <input type="text" class="form-control" id="basicpill-servicetax-input">
-                            </div>
+                    <div class="row mb-3">
+                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" placeholder="Alamat" id="alamat"
+                                name="alamat">
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-companyuin-input">Company
-                                    UIN</label>
-                                <input type="text" class="form-control" id="basicpill-companyuin-input">
-                            </div>
-                        </div>
 
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-declaration-input">Declaration</label>
-                                <input type="text" class="form-control" id="basicpill-declaration-input">
-                            </div>
+                    <div class="row mb-3">
+                        <label for="telepon" class="col-sm-2 col-form-label">Telepon</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="tel" placeholder="Telepon" id="telepon"
+                                name="telepon">
                         </div>
                     </div>
                 </form>
@@ -129,70 +165,62 @@
         <div class="tab-pane" id="bank-detail">
             <div>
                 <form>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-namecard-input">Name
-                                    on Card</label>
-                                <input type="text" class="form-control" id="basicpill-namecard-input">
-                            </div>
-                        </div>
-
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label>Credit Card Type</label>
-                                <select class="form-select">
-                                    <option selected>Select Card Type</option>
-                                    <option value="AE">American Express</option>
-                                    <option value="VI">Visa</option>
-                                    <option value="MC">MasterCard</option>
-                                    <option value="DI">Discover</option>
-                                </select>
-                            </div>
+                    <div class="row mb-3">
+                        <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" placeholder="Nama" id="nama"
+                                name="nama">
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-cardno-input">Credit
-                                    Card Number</label>
-                                <input type="text" class="form-control" id="basicpill-cardno-input">
-                            </div>
-                        </div>
 
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-card-verification-input">Card
-                                    Verification
-                                    Number</label>
-                                <input type="text" class="form-control" id="basicpill-card-verification-input">
-                            </div>
+                    <div class="row mb-3">
+                        <label for="jenis_identitas" class="col-sm-2 col-form-label">Jenis Identitas</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" placeholder="Jenis Identitas"
+                                id="jenis_identitas" name="jenis_identitas">
                         </div>
                     </div>
-                    <div class="row">
-                        <div class="col-lg-6">
-                            <div class="mb-3">
-                                <label class="form-label" for="basicpill-expiration-input">Expiration
-                                    Date</label>
-                                <input type="text" class="form-control" id="basicpill-expiration-input">
-                            </div>
-                        </div>
 
+                    <div class="row mb-3">
+                        <label for="jabatan" class="col-sm-2 col-form-label">Jabatan</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" placeholder="Jabatan" id="jabatan"
+                                name="jabatan">
+                        </div>
+                    </div>
+
+                    <div class="row mb-3">
+                        <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" placeholder="Alamat" id="alamat"
+                                name="alamat">
+                        </div>
                     </div>
                 </form>
             </div>
         </div>
         <div class="tab-pane" id="confirm-detail">
-            <div class="row justify-content-center">
-                <div class="col-lg-6">
-                    <div class="text-center">
-                        <div class="mb-4">
-                            <i class="mdi mdi-check-circle-outline text-success display-4"></i>
+            <div>
+                <div class="row mb-4">
+                    <div class="d-flex justify-content-start">
+                        <label class="col-md-4 col-sm-7">Apakah terdapat yang diberi kuasa?</label>
+                        <div class="col-md-1 col-sm-2 ms-4">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="kuasa" id="kuasa_ya"
+                                    value="ya">
+                                <label class="form-check-label" for="kuasa_ya">
+                                    Ya
+                                </label>
+                            </div>
                         </div>
-                        <div>
-                            <h5>Confirm Detail</h5>
-                            <p class="text-muted">If several languages coalesce, the grammar of
-                                the resulting</p>
+                        <div class="col-md-1 col-sm-2 ms-3">
+                            <div class="form-check">
+                                <input class="form-check-input" type="radio" name="kuasa" id="kuasa_tidak"
+                                    value="tidak">
+                                <label class="form-check-label" for="kuasa_tidak">
+                                    Tidak
+                                </label>
+                            </div>
                         </div>
                     </div>
                 </div>
