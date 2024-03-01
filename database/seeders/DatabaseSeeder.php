5e20ad99264991233a858601dea692fd6e4d1d3c
Npservice <110673469+Npservice@users.noreply.github.com>
2024-03-02 02:32:18 +0700

    test

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -19,7 +19,7 @@ class DatabaseSeeder extends Seeder
             MasterProvinsiSeeder::class,
             MasterKotaKabSeeder::class,
             PjBaratanKppSeeder::class,
-            PjBaratinSeeder::class
+            // PjBaratinSeeder::class
         ]);
     }
 }
