f850b6efb14ff6c1c36a00cc523694781feb4349
Npservice <110673469+Npservice@users.noreply.github.com>
2024-03-02 02:34:35 +0700

    test

--- resources/views/admin/baratin/action.blade.php
+++ resources/views/admin/baratin/action.blade.php
@@ -1,8 +1,4 @@
-<a class="btn btn-outline-warning btn-sm me-2"
-    onclick="modal('Edit UPT','modal-xl','static','{{ route('admin.master-upt.edit', $model->id) }}')" title="Edit"><i
-        class="fas fa-edit"></i></a>
-<a class="btn btn-outline-success btn-sm me-2"
-    onclick="DeleteAlert('{{ route('admin.master-upt.destroy', $model->id) }}','{{ $model->nama }}')"><i
-        class="fas fa-check"></i></a>
+<a class="btn btn-outline-warning btn-sm me-2" onclick="" title="Edit"><i class="fas fa-edit"></i></a>
+<a class="btn btn-outline-success btn-sm me-2" onclick=""><i class="fas fa-check"></i></a>
 <a class="btn btn-outline-info btn-sm" onclick="ShowPage('{{ route('admin.baratin.show', $model->id) }}')"><i
         class="fas fa-eye"></i></a>
