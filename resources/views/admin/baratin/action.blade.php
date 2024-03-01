b1a2e4f2a919e0d4332e142a8d1ac7703fb46759
Npservice <110673469+Npservice@users.noreply.github.com>
2024-03-02 02:43:14 +0700

    test

--- resources/views/admin/baratin/action.blade.php
+++ resources/views/admin/baratin/action.blade.php
@@ -1,4 +1,5 @@
 <a class="btn btn-outline-warning btn-sm me-2" onclick="" title="Edit"><i class="fas fa-edit"></i></a>
 <a class="btn btn-outline-success btn-sm me-2" onclick=""><i class="fas fa-check"></i></a>
+
 <a class="btn btn-outline-info btn-sm" onclick="ShowPage('{{ route('admin.baratin.show', $model->id) }}')"><i
         class="fas fa-eye"></i></a>
