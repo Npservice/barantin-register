6882ee3ffe850f6ef1f3a200240c31e55b570f7a
Npservice <nandaputraservice@gmail.com>
2024-03-30 22:48:58 +0700

    kernel update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -64,6 +64,7 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
         'ajax' => \App\Http\Middleware\AjaxRequest::class,
+        'induk' => \App\Http\Middleware\IndukPerusahan::class,
         'registerfailed' => \App\Http\Middleware\RestricRegisterFailedMidlleware::class
     ];
 }
