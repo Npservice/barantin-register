abf8f41d9e350763941878e08f7a8687d484e50a
Npservice <nandaputraservice@gmail.com>
2024-05-31 18:55:53 +0700

    update logic action

--- resources/views/user/cabang/action.blade.php
+++ resources/views/user/cabang/action.blade.php
@@ -1,4 +1,4 @@
     <a class="btn btn-outline-primary btn-sm" onclick="ShowPage('{{ route('barantin.cabang.show', $model->id) }}')"><i class="fas fa-eye"></i></a>
-    @if ($model->persetujuan_induk != 'DISETUJUI')
+    @if ($model->persetujuan_induk == 'MENUNGGU')
         <a class="btn btn-outline-success btn-sm xe-2" onclick="ConfirmCabang('{{ route('barantin.cabang.confirmasi', $model->id) }}', '{{ $model->nama_perusahaan }}')"><i class="fas fa-check"></i></a>
     @endif
